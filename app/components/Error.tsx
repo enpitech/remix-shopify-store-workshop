9b4a02203366b3ec4bcce40e4c73870951df33cf
Tomer Oz Fresko <54535934+Tomerozfresko@users.noreply.github.com>
2022-12-23 14:50:47 +0100

    adding ErrorBoundary function

---
+++ app/components/Error.tsx
@@ -0,0 +1,23 @@
+export default function Error({ error }) {
+  return (
+    <>
+      <div className="min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div className="mx-auto max-w-max">
+          <main className="sm:flex">
+            <p className="text-4xl font-bold tracking-tight text-indigo-600 sm:text-5xl">
+              Error:
+            </p>
+            <div className="sm:ml-6">
+              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                  {error.message}
+                </h1>
+              </div>
+              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6"></div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
